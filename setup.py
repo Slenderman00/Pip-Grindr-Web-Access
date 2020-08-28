7ca4fcd1f7d13c747e8360be97e928d2a338b0a6
Joar <joarheimonen@live.no>
2020-08-28 18:26:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='GrindrWebAccsess',
     version='0.0.1',
     description='A framework for using the github v4 api in python',
-    url='git@github.com:slenderman00/Pip-Grindr-Web-Accsess.git',
-    author='Raphael Schubert',
+    url='git@github.com:slenderman00/Pip-Grindr-Web-Access.git',
+    author='Joar Heimonen',
     author_email='joarheimonen@live.no',
     license='mit',
-    packages=['Grindr-Web-Accsess'],
+    packages=['Grindr-Web-Access'],
     zip_safe=False
 )
